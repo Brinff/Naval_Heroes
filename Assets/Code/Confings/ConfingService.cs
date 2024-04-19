f5d0e60f17950e42bb4443929f602339caaf0fe4
Home PC <vitalik1994.tukmachyov@gmail.com>
2024-04-19 14:08:03 +0300

    Add ui animate button

--- Assets/Code/Confings/ConfingService.cs
+++ Assets/Code/Confings/ConfingService.cs
@@ -36,28 +36,13 @@ namespace Code.Confings
 
         [OdinSerialize]
         private Data m_Data = new Data();
-        
-        //[SerializeField, DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.OneLine)]
-        //private Dictionary<string, IConfigurableProperty> m_Data = new Dictionary<string, IConfigurableProperty>();
-        
+
         [Button]
         public void Save()
         {
-            //Debug.Log(JsonConvert.SerializeObject(m_Data));
             var value = SerializationUtility.SerializeValue(m_Data, DataFormat.JSON);
             var text = Encoding.UTF8.GetString(value).Replace(" ", "").Replace("\n", "");
             Clipboard.Copy(text);
-            //Debug.Log( );
-            //Sirenix.Serialization.Serializer s = new AnySerializer(typeof(Data));
-            //Stream stream = new MemoryStream();
-            //var contex = new SerializationContext();
-            //var jsonWriter = new JsonDataWriter(stream, contex);
-            //s.WriteValueWeak(m_Data, jsonWriter);
-
-
-            //Sirenix.Serialization.od
-            //string jsonText = JsonUtility.ToJson(m_Data, true);
-            //Debug.Log(jsonWriter.Stream.);
         }
 
         [Button]
---
+++ Assets/Code/Game/Slots/BuyShipService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Code.Game.Wallet;
+using Code.Services;
+using UnityEngine;
+
+namespace Code.Game.Slots
+{
+    public class BuyShipService : MonoBehaviour, IService, IInitializable
+    {
+        [SerializeField] private ProgressionData m_ProgressionCost;
+        [SerializeField] private ClassificationData m_Classification;
+        public ClassificationData classification => m_Classification;
+        [SerializeField] private Currency m_Currency;
+
+        private WalletService m_WalletService;
+
+        private void OnEnable()
+        {
+            ServiceLocator.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            ServiceLocator.Unregister(this);
+        }
+
+
+        public bool IsEnoughCurrency()
+        {
+            return m_WalletService.IsEnough((int)m_ProgressionCost.GetResult(1));
+        }
+        
+        public void Initialize()
+        {
+            m_WalletService = ServiceLocator.Get<WalletService>(x => x.currency == m_Currency);
+        }
+    }
+}
